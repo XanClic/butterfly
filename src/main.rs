43bf5f37fa85dc52bcea8128968179aaafd508a4
Sophie Reitz <sophie@xanclic.moe>
2018-01-22 02:28:56 +0100

    Use realpath to identify files in config.json

--- src/main.rs
+++ src/main.rs
@@ -32,17 +32,26 @@ fn main() {
         exit(1);
     }
 
+    let path = match std::fs::canonicalize(std::path::Path::new(&argv[1])) {
+        Ok(p)   => p,
+        Err(e)  => { eprintln!("Failed to find {}: {}", argv[1], e); exit(1) }
+    };
+    let path_str = match path.to_str() {
+        Some(s) => String::from(s),
+        None    => { eprintln!("Invalid path {}", argv[1]); exit(1) }
+    };
+
     let mut config = match ConfigFile::new() {
         Ok(f)   => f,
         Err(e)  => { eprintln!("Failed to open config file: {}", e); exit(1) }
     };
 
-    let file = match File::new(argv[1].clone()) {
+    let file = match File::new(path_str.clone()) {
         Ok(f)   => f,
         Err(e)  => { eprintln!("Failed to open: {}", e); exit(1) }
     };
 
-    let undo_file = match UndoFile::new(&mut config, argv[1].clone()) {
+    let undo_file = match UndoFile::new(&mut config, path_str.clone()) {
         Ok(f)   => f,
         Err(e)  => { eprintln!("Failed to open undo file: {}", e); exit(1) }
     };
